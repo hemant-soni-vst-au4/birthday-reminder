13f439afb05c03715be0469a0627543b2256fa1a
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-08 21:36:56 +0530

    state added in list component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 
 const App = () => {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthday today</h3>
-        <List />
+        <h3>{people.length} birthday today</h3>
+        <List people={people}/>
         <button onClick={() => console.log("you clicked")}>clear all</button>
-      </section>
+      </section>   
     </main>
   );
 };
